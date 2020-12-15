6f5979897c3a1f976faf512ec0ceb992e1f26fa7
colin <zephyr.raine@gmail.com>
2020-12-15 17:42:42 +0100

    ModifTir

--- Assets/Tir.cs
+++ Assets/Tir.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class Tir : MonoBehaviour
 {
-    public int SpeedRotation;
+    [SerializeField]
+    Transform directionGizmo;
+
+    [SerializeField]
+    float forceTir;
+
+    Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = GameObject.Find("Balle").GetComponent<Transform>().position;
-        transform.eulerAngles = Vector3.up * Input.GetAxis("Mouse X");
+        
+        //transform.eulerAngles = Vector3.up * Input.GetAxis("Mouse X");
+
+        Vector3 positionCamera = mainCamera.transform.position;
+        positionCamera.y = transform.position.y;
+        
+        //Direction que doit prendre la balle
+        Vector3 camBalle = (transform.position - positionCamera).normalized;
+
+
+        Vector3 positionDirection = transform.position + camBalle * 3f;
+        directionGizmo.LookAt(positionDirection);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody>().AddForce(camBalle * forceTir, ForceMode.Impulse);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        directionGizmo.position = transform.position;
     }
 }
